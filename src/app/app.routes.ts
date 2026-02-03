c639bc6be4e56f6224a5d0b412d8b0703b710be6
Ulises Santiago Caceres Licona <uliseo_95@hotmail.com>
2026-02-02 22:19:40 -0600

    Se integra nvar component y documento .json faltante

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { navComponent } from './components/nav/nav-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+        {
+               path: '',component: navComponent 
+        }
+];
